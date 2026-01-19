518a37c863eff9c7e820bf085d495542e4c358be
Clay Smith <smithclay@gmail.com>
2026-01-18 21:36:24 -0800

    fix: gate local filesystem module behind non-WASM cfg

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -2,7 +2,9 @@
 //! WASM-compatible via OpenDAL
 
 mod file_io;
+#[cfg(not(target_family = "wasm"))]
 pub mod local;
 
 pub use file_io::{AwsCredentials, FileIO, VendedCredentialProvider, VendedCredentials};
+#[cfg(not(target_family = "wasm"))]
 pub use local::{create_local_file_io, get_filename, is_local_path};
